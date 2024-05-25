eece538d39c38672dd3dda4d243e4cd7ab483024
cantdocpp <cihuyman95@gmail.com>
2024-05-25 21:32:10 +0700

    add node installer

--- install.sh
+++ install.sh
@@ -61,3 +61,18 @@ EOF
     echo "User: $PG_USER"
     echo "Database: $PG_DATABASE"
 fi
+
+if ! [ -x "$(command -v node)" ]; then
+    # installs nvm (Node Version Manager)
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    # download and install Node.js
+    nvm install 20
+    # verifies the right Node.js version is in the environment
+    node -v # should print `v20.13.1`
+    # verifies the right NPM version is in the environment
+    npm -v # should print `10.5.2`
+fi
+
+if ! [ -x "$(command -v pm2)" ]; then
+    npm install pm2 -g
+fi
