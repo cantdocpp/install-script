35fa21efb821d6ed1ed1b13e90707cd6446b265c
cantdocpp <cihuyman95@gmail.com>
2024-05-25 18:36:05 +0700

    add postgresql installer

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DOCKER_VERSION="26.0"
 
 OS_TYPE=$(grep -w "ID" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
@@ -21,6 +23,12 @@ echo -e "-------------"
 echo "OS: $OS_TYPE $OS_VERSION"
 
 if ! [ -x "$(command -v docker)" ]; then
+    echo -e "Installing docker"
     curl https://releases.rancher.com/install-docker/${DOCKER_VERSION}.sh | sh
 fi
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo -e "Installing postgresql"
+    sudo apt install -y postgresql-common
+    sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+fi
