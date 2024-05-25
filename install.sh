dddae648ed941d43e17e23b2604c7a4b96120981
cantdocpp <cihuyman95@gmail.com>
2024-05-25 18:20:22 +0700

    add docker installation

---
+++ install.sh
@@ -0,0 +1,26 @@
+DOCKER_VERSION="26.0"
+
+OS_TYPE=$(grep -w "ID" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+OS_VERSION=$(grep -w "VERSION_ID" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+
+if [ $EUID != 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+if [ "$OS_TYPE" != 'ubuntu' ]; then
+    echo "Script can only be run at ubuntu machine"
+    exit
+fi
+
+echo -e "-------------"
+echo -e "Running installation scripts..."
+echo -e "(Source code: https://github.com/gboengineering/gbo-scripts)\n"
+echo -e "-------------"
+
+echo "OS: $OS_TYPE $OS_VERSION"
+
+if ! [ -x "$(command -v docker)" ]; then
+    curl https://releases.rancher.com/install-docker/${DOCKER_VERSION}.sh | sh
+fi
+
