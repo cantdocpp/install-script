a8453772617ef69b1830db7da9bed994acbd6cb7
cantdocpp <cihuyman95@gmail.com>
2024-05-25 18:46:40 +0700

    update postgresql scripts

--- install.sh
+++ install.sh
@@ -29,10 +29,10 @@ fi
 
 if ! [ -x "$(command -v psql)" ]; then
     echo -e "Installing postgresql"
-    sudo apt install -y postgresql-common
-    sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-    
+    sudo apt install curl ca-certificates
+    sudo install -d /usr/share/postgresql-common/pgdg
+    sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+    sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+    sudo apt update
+    sudo apt -y install postgresql
 fi
-
-sudo apt-get update
-sudo apt-get install postgresql-client-16
